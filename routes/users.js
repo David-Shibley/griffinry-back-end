474b28ddc6f77057a8810b53d1cc2cf419c67642
David-Shibley <davidjshibley@gmail.com>
2016-01-10 21:18:15 -0700

    set up and linked route for play profiles, needs styling and content layout still

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var knex = require('../db/knex');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+function Users() {
+  return knex('Users');
+}
+
+router.get('/:id', function(req, res) {
+  Users().select().where('id', req.params.id).first()
+  .then(function(profile) {
+    res.render('profile', {
+      user_data: profile
+    });
+  });
 });
 
 module.exports = router;
