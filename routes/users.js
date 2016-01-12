00cac41a193b61267829251c57536fe28ac8575d
David-Shibley <davidjshibley@gmail.com>
2016-01-12 09:40:36 -0700

    tried getting pet type but not working yet. switching branches to work on something else.

--- routes/users.js
+++ routes/users.js
@@ -10,22 +10,26 @@ function Adoptions() {
   return knex('adoptions');
 }
 
+function Pets() {
+  return knex('pets');
+}
+
 router.get('/:id', function(req, res) {
   Users().where('users.id', req.params.id)
   .innerJoin('adoptions', 'adoptions.User_Id', 'users.id')
-  .select('users.User_Name', 'users.DOB', 'users.id', 'adoptions.Name', 'adoptions.Color', 'adoptions.id as adoptions_id')
+  .select('users.User_Name', 'users.DOB', 'users.id', 'adoptions.Name', 'adoptions.Pet_Id', 'adoptions.Color', 'adoptions.id as adoptions_id')
   // .unionAll(function() {
   //   Adoptions().innerJoin('pets', 'pets.id','adoptions.Pet_Id')
   //   .select('pets.id as pets_id')
   // })
   .then(function(adoptions) {
-    console.log(adoptions);
-    // Adoptions().where('User_Id', profile.).then(function(adoptions) {
+    Pets().where('id', adoptions.Pet_Id)
+    .then(function(pet) {
       res.render('profile', {
         user_data: adoptions,
-        // adoptions: adoptions
+        user_pet: pet
       });
-    // });
+    });
   });
 });
 
