cce9996ab98abdd6383ba5b8dd54c3c192d056e5
J. Drill <JJDrill@hotmail.com>
2016-01-12 21:58:48 -0700

    Added feed route for adoptions

--- routes/adoptions.js
+++ routes/adoptions.js
@@ -11,6 +11,18 @@ function Adoptions(){
   return knex('adoptions')
 };
 
+function getPetMaxHealth(adoptionId){
+  return Adoptions().where('id', adoptionId).select('Max_Health').first()
+};
+
+function getPetCurrentHealth(adoptionId){
+  return Adoptions().where('id', adoptionId).select('Current_Health').first()
+};
+
+function increasePetHealth(adoptionId, amount){
+  return Adoptions().where('id', adoptionId).update('Current_Health', amount)
+}
+
 router.post('/add', function(req, res){
   Adoptions().insert({
     User_Id: req.body.userId,
@@ -30,19 +42,36 @@ router.post('/add', function(req, res){
 });
 
 router.get('/list/:id', function(req, res){
-  Adoptions().select().where('User_Id', req.params.id)
-  .then(function(list){
+  Adoptions().select().where('User_Id', req.params.id).then(function(list){
     res.send(list);
   })
 });
 
 router.delete('/delete/:id', function(req, res){
-  Adoptions()
-  .where('id', req.params.id)
-  .del()
-  .then(function(result){
+  Adoptions().where('id', req.params.id).del().then(function(result){
     res.sendStatus(result);
   });
+});
+
+router.get('/feed/:id', function(req, res){
+  var maxHealth = 0;
+  var currentHealth = 0;
+
+  getPetMaxHealth(req.params.id).then(function(result){
+    maxHealth = result.Max_Health
+  }).then(function(){
+    getPetCurrentHealth(req.params.id).then(function(result){
+      currentHealth = result.Current_Health;
+      if (currentHealth < maxHealth) {
+        currentHealth += 1;
+        increasePetHealth(req.params.id, currentHealth).then(function(){
+          res.send(currentHealth);
+        });
+      } else {
+        res.send('Pet at max health: ' + maxHealth)
+      }
+    })
+  })
 });
 
 module.exports = router;
