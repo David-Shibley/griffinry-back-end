112f44c7e8f7da82142a7f57863c4b99f38b4e19
David-Shibley <davidjshibley@gmail.com>
2016-01-12 18:40:38 -0700

    added an api that serves up current session user id

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var resources = require('./routes/resources');
 var adoptions = require('./routes/adoptions');
 var pets = require('./routes/pets');
 var dashboard = require('./routes/dashboard');
+var userId = require('./routes/api');
 
 require('dotenv').load();
 
@@ -94,6 +95,7 @@ app.use('/resources', resources);
 app.use('/adoptions', adoptions);
 app.use('/pets', pets);
 app.use('/dashboard', dashboard);
+app.use('/userId', userId);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
